b78a44076400cb9c7b6978564c3c4807acc61c1d
stanchan <stanchanpsu@outlook.com>
2017-11-23 00:36:38 -0500

    Create bubble.js

---
+++ sort/bubble.js
@@ -0,0 +1,18 @@
+function bubbleSort(list) {
+    var n = list.length - 1;
+    var notSorted = false;
+    do {
+        notSorted = false;
+        for (var i = 0; i < n; i++) {
+            if (list[i] > list[i+1]) {
+                let temp = list[i];
+                list[i] = list[i+1];
+                list[i+1] = temp;
+                notSorted = true;
+            }
+        }
+    } while (notSorted)
+    return list;
+}
+
+exports.bubbleSort = bubbleSort;
